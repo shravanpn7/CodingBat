031409ec4a29cae3fdb402f8e20b67c227727b47
Shravan Papanaidu <shravanpn7@gmail.com>
2015-07-25 19:09:31 -0700

    Adding solution for make_tags

--- String-1/make_tags.py
+++ String-1/make_tags.py
@@ -1,2 +1,17 @@
 __author__ = 'Shravan Papanaidu'
 
+###Question
+
+
+# The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+#
+# make_tags('i', 'Yay') ? '<i>Yay</i>'
+# make_tags('i', 'Hello') ? '<i>Hello</i>'
+# make_tags('cite', 'Yay') ? '<cite>Yay</cite>'
+
+def make_tags(tag, word):
+    tag_start = '<'+tag+'>'
+    tag_end = '</'+tag+'>'
+    return tag_start+word+tag_end
+
+print make_tags('i', 'Wow')
