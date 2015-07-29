4d62c4a9b6274b8a59c8b5c9140d0536dfe3a093
Shravan Papanaidu <shravanpn7@gmail.com>
2015-07-28 21:36:32 -0700

    Adding solution for same_first_last

--- List-1/same_first_last.py
+++ List-1/same_first_last.py
@@ -5,3 +5,10 @@ __author__ = 'Shravan Papanaidu'
 # same_first_last([1, 2, 3, 1]) ? True
 # same_first_last([1, 2, 1]) ? True
 
+def same_first_last(nums):
+    if len(nums) >=1 and nums[0] == nums[-1]:
+        return True
+    else:
+        return False
+
+print same_first_last([1,2,3,1])
