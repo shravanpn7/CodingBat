4f7727ea5bcc703e72a417784f10eeea5114593c
Shravan Papanaidu <shravanpn7@gmail.com>
2015-07-28 21:39:17 -0700

    Adding solution for make_pi

---
+++ List-1/make_pi.py
@@ -0,0 +1,10 @@
+__author__ = 'Shravan Papanaidu'
+
+# Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+#
+# make_pi() ? [3, 1, 4]
+
+def make_pi():
+    return [3,1,4] # Seriously?
+
+print make_pi()
