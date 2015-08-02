1ad86013df5d1675c4b264e0f0cfd2af7d8380f2
Shravan Papanaidu <shravanpn7@gmail.com>
2015-08-02 11:41:56 -0700

    Adding solution for sorta_sum

---
+++ Logic-1/sorta_sum.py
@@ -0,0 +1,17 @@
+__author__ = 'Shravan Papanaidu'
+
+
+# Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+#
+# sorta_sum(3, 4) ? 7
+# sorta_sum(9, 4) ? 20
+# sorta_sum(10, 11) ? 21
+
+def sorta_sum(a,b):
+    sum = a+b
+    if sum in range(10,20):
+        return 20
+    else:
+        return sum
+
+print sorta_sum(10,3)
